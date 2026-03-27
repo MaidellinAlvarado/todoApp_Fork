3b3f6b363cc7b8edcff1337b8309da168471fa92
Maidellin Alvarado <24011377@galileo.edu>
2026-03-26 19:52:25 -0600

    feat(security): add Helmet, CORS, dotenv, payload limit - Clase 4

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
-
 require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
-app.use(helmet());
-const cors = require('cors');
+const cors = require('cors'); 
 const tareasRouter = require('./routes/tareas');
 const authRouter = require('./routes/auth');
-const app = express();
 
+
+// 1. PRIMERO  LA APP
+const app = express(); 
+
+// 2. ESCUDOS
+app.use(helmet());
 
 // capa 2: CONTROL DE ORIGENES
 app.use(cors({
@@ -19,9 +22,10 @@ app.use(cors({
 // capa 3: limite del payload
 app.use(express.json({ limit: '10kb' }));
 
+// 3. RUTAS
+
 app.use('/api/auth', authRouter);
 app.use('/api/tareas', tareasRouter);
-
 
 app.get('/', (req, res) => res.json({ ok: true }));
 
--- src/server.js
+++ src/server.js
@@ -1,15 +1,25 @@
-
 require('dotenv').config();
 const mongoose = require('mongoose');
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
-const MONGO_URI = process.env.MONGO_URI;
+
+
+// En tu server.js
+const MONGO_URI = process.env.MONGODB_URI;
+
+console.log('--- Intentando conectar a:', MONGO_URI);
+
+if (!MONGO_URI) {
+    console.error('ERROR: La variable MONGODB_URI es undefined. Revisa tu archivo .env');
+    process.exit(1);
+}
 
 mongoose.connect(MONGO_URI)
-.then(() => {
-app.listen(PORT, () => {
-console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
-})
-.catch(err => console.error(err));
+  .then(() => {
+    console.log('✅ Conectado a MongoDB');
+    app.listen(PORT, () => {
+      console.log(`🚀 Servidor en http://localhost:${PORT}`);
+    });
+  })
+  .catch(err => console.error('❌ Error de conexión:', err.message));
